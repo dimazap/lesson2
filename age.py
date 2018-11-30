6f80ada89f5edc84531797d6214d7e8da63de78a
Dima Zaplavsky <dima.zap@gmail.com>
2018-11-30 16:45:47 +0100

    Age, Strings and Question tasks added

---
+++ age.py
@@ -0,0 +1,11 @@
+age = int(input("What is your age? "))
+print(f"You are {age} years old")
+
+if age in range(0, 7):
+    print('Hi, you are in kindergarden')
+elif age in range(7, 19):
+    print('Hi, you are a pupil')
+elif age in range(19, 25):
+    print('Hi, you are a student')
+else:
+    print('Hi, you are a worker')
