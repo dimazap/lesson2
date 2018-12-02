4dce017ec7998db09dc31953b6ded6a345facf52
Dima Zaplavsky <dima.zap@gmail.com>
2018-12-02 14:07:20 +0100

    Added infinite loop for age.py

--- ask.py
+++ ask.py
@@ -1,9 +1,10 @@
 def main():
-    talk = {"Как дела?": "Хорошо!", "Что делаешь?": "Программирую", "Что будешь делать?": "Программировать"}
-    keyinput = input("Каков твой вопрос? ")
-    if keyinput in talk.keys():
-        print(talk[keyinput])
-    else:
-        print("Тогда до свиданья")
+	talk = {"Как дела?": "Хорошо!", "Что делаешь?": "Программирую", "Что будешь делать?": "Программировать"}
+	while True:
+		keyinput = input("Каков твой вопрос? ")
+		if keyinput in talk.keys():
+			print(talk[keyinput])
+		else:
+			print("Тогда до свиданья")
 
 main()
---
+++ ask_old.py
@@ -0,0 +1,9 @@
+def main():
+    talk = {"Как дела?": "Хорошо!", "Что делаешь?": "Программирую", "Что будешь делать?": "Программировать"}
+    keyinput = input("Каков твой вопрос? ")
+    if keyinput in talk.keys():
+        print(talk[keyinput])
+    else:
+        print("Тогда до свиданья")
+
+main()
